12cef677d84e716d55dece7542dce49b4eb242b5
Andrey Bekhterev <abehterev@gmail.com>
2015-03-24 17:25:19 +0300

    Add send_pkt.h

---
+++ send_pkt.h
@@ -0,0 +1,22 @@
+#ifndef SEND_PKT_H
+#define SEND_PKT_H
+
+/* Broadcast Destination MAC */
+#define MY_DEST_MAC0 0xff
+#define MY_DEST_MAC1 0xff
+#define MY_DEST_MAC2 0xff
+#define MY_DEST_MAC3 0xff
+#define MY_DEST_MAC4 0xff
+#define MY_DEST_MAC5 0xff
+
+/* Source MAC */
+#define MY_S_MAC0 0x00
+#define MY_S_MAC1 0x01
+#define MY_S_MAC2 0x02
+#define MY_S_MAC3 0x03
+
+#define BUF_SIZE 70
+
+#define SleepTime 50000 // microseconds
+
+#endif // send_pkt.h
